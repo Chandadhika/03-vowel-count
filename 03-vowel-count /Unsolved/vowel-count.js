8d144316b834202e8d2d5f75854e123b17e7cae9
ANarngAung <anarngaung2-17@gmail.com>
2024-05-28 23:11:59 +0630

    Modified script file

--- 03-vowel-count /Unsolved/vowel-count.js
+++ 03-vowel-count /Unsolved/vowel-count.js
@@ -1,18 +1,37 @@
 // Write code to return the the number of vowels in `str`
 
+// var vowelCount = function(str) {
+//     var result = 0;
+//     var vowels = ["a", "e", "i", "o", "u"];
+
+//     for (var i = 0; i < str.length; i++) {
+//         var letter = str[i].toLowerCase();
+
+//         if (vowels.indexOf(letter) !== -1) {
+//             result += 1;
+//         }
+//     }
+//     return result;
+// };
+
+
+
+
+// alternatively, this problem could have been solved without the use of indexOf, but by using the logical OR (||) operator to check for each vowel
 var vowelCount = function(str) {
     var result = 0;
-    var vowels = ["a", "e", "i", "o", "u"];
 
     for (var i = 0; i < str.length; i++) {
         var letter = str[i].toLowerCase();
 
-        if (vowels.indexOf(letter) !== -1) {
+        if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
             result += 1;
         }
     }
     return result;
 };
+
+
 
 
 
